78289fed1f31b0e9289ae0027beccaa4378a15f3
Vidhi Jadav <vjadav@mail.usf.edu>
2020-01-21 20:59:12 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 24;
+            int height = 162;
+            int max_heart_rate;
+            max_heart_rate = 220 - age;
+            Console.WriteLine("I am " + age + " years old, my max heart rate is " + max_heart_rate + " and I am " + height + " inches tall.");
+        }
+    }
+}
